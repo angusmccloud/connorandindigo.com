9d62bb3929d8e5dc5efc250b08df82c8d95b6e98
Connor Tyrrell <connor@brewgene.com>
2022-01-17 17:51:49 -0500

    Basic slideshow for year in review

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import ReactGoogleSlides from "react-google-slides";
+
+function App() {
+  return (
+    <div style={{height: '100vh'}}>
+      <ReactGoogleSlides
+        width={'100%'}
+        height={'100%'}
+        slidesLink="https://docs.google.com/presentation/d/1sGDpOobCcTgwb4I1iHs0nJZW_ogEsmp4VifuG3c_l28"
+        slideDuration={300}
+        position={1}
+        // showControls={true}
+        loop={true}
+      />
+    </div>
+  );
+}
+
+export default App;
